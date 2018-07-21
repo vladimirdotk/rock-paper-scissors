0b3f6195cd2196d94dbc79caf1cfccb1115b143f
demothreen <demonnn@list.ru>
2018-07-21 21:28:05 +0500

    another style for the game

--- src/GameOver.js
+++ src/GameOver.js
@@ -3,13 +3,13 @@ import React from 'react';
 const CHOICES = ['rock', 'paper', 'scissors'];
 
 const GameOver = ({playerChoice, computerChoice, result, handleGameOverBtn}) => {
-  return (
-    <div style={{"marginTop": "20px"}}>
-      You choose {CHOICES[playerChoice]}, computer chooses {CHOICES[computerChoice]}.
-      <div style={{"marginTop": "20px"}}> Result: {result}!</div>
-      <button style={{"marginTop": "20px"}} onClick={handleGameOverBtn}>OK</button>
-    </div>
-  );
+    return (
+        <div className="result">
+            You choose {CHOICES[playerChoice]}, computer chooses {CHOICES[computerChoice]}.
+            <div> Result: {result}!</div>
+            <button className="restart" onClick={handleGameOverBtn}>OK</button>
+        </div>
+    );
 }
 
 export default GameOver;
